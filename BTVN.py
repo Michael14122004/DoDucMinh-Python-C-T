5da3167f0ec30feede2fcaba1ed6aba19183adf9
Michael14122004 <52208318+Michael14122004@users.noreply.github.com>
2019-07-03 22:04:54 +0700

    Create BTVN.py

---
+++ BTVN.py
@@ -0,0 +1,9 @@
+# DoDucMinh-Python-C-T
+print("Hi")
+print("Tuoi ban la bao nhieu? ")
+tuoi = input()
+
+if int(tuoi) > 18:
+    print("Moi ban xem tiep")
+else:
+    print("Ban chua du tuoi de xem noi dung nay")
