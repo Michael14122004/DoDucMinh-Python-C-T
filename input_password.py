b744b36ccd8939a265a34aed16759dadb98353bc
Michael14122004 <52208318+Michael14122004@users.noreply.github.com>
2019-07-08 22:49:12 +0700

    Create input_password.py

---
+++ input_password.py
@@ -0,0 +1,8 @@
+# DoDucMinh-Python-C-T
+while True:
+    so = input("Enter your password")
+    if so.isdigit():
+        print("OK")
+        break
+    else:
+        print("PASSWORD NEED NUMBER NOT WORD")
